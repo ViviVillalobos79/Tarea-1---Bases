49db2c6f82b1c4d2afbcff2407e6d01b6cc83b07
ViviVillalobos79 <vvillalobosv@estudiantec.cr>
2020-10-21 23:21:15 -0600

    vista productor lista

--- TareaCorta1/src/app/Components/productor/productor.component.ts
+++ TareaCorta1/src/app/Components/productor/productor.component.ts
@@ -1,31 +1,35 @@
-import {Component} from '@angular/core'
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-    selector: 'productor',
-    templateUrl: './productor.component.html',
-    styleUrls: ['./productor.component.css']
+  selector: 'productor',
+  templateUrl: './productor.component.html',
+  styleUrls: ['./productor.component.css'],
 })
+export class Productor {
+  cedula: string;
+  i = 0;
+  img = [
+    '../../../assets/Images/Image1.png',
+    '../../../assets/Images/Image2.png',
+    '../../../assets/Images/Image5.png',
+    '../../../assets/Images/Image6.png',
+    '../../../assets/Images/Image30.png',
+  ];
 
-export class Productor{
-    i=0;
-    img=["../../../assets/Images/Image1.png",
-            '../../../assets/Images/Image2.png',
-            '../../../assets/Images/Image5.png',
-            '../../../assets/Images/Image6.png',
-            '../../../assets/Images/Image30.png']
+  constructor(private _route: ActivatedRoute) {
+    console.log(this._route.snapshot.paramMap.get('id'));
+      this.cedula = this._route.snapshot.paramMap.get('id');
+  }
+  left() {
+    if (this.i > 0) {
+      this.i = this.i - 1;
+    }
+  }
 
-    
-    constructor(){}
-    left() {
-        if (this.i > 0) {
-          this.i = this.i - 1;
-        }
-      }
-    
-      right() {
-        if (this.i < 4) {
-          this.i = this.i + 1;
-        }
-      }
-    
+  right() {
+    if (this.i < 4) {
+      this.i = this.i + 1;
+    }
+  }
 }
